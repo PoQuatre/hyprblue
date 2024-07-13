50a9f318d93697d967552db677e97ee5db1ed788
PoQuatre <leflem.mateo2002@gmail.com>
2024-07-13 05:45:24 +0200

    feat: add the minimal set of packages needed for hyprland

--- build.sh
+++ build.sh
@@ -4,20 +4,12 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
+# Install packages
 
-### Install packages
+rpm-ostree install hyprland xdg-desktop-portal-hyprland kitty
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+rpm-ostree override remove vim --install neovim
 
-# this installs a package from fedora repos
-rpm-ostree install screen
-
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
-
-#### Example for enabling a System Unit File
+# Enabling System Units
 
 systemctl enable podman.socket
