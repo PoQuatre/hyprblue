a4e089486da1a5e1438f03bd256d47831351d7ad
PoQuatre <leflem.mateo2002@gmail.com>
2024-08-05 11:45:03 +0200

    fix: add symlinks with higher priority from `vim` to `nvim`

--- build.sh
+++ build.sh
@@ -6,8 +6,12 @@ RELEASE="$(rpm -E %fedora)"
 
 # Install packages
 
-rpm-ostree uninstall vim
 rpm-ostree install hyprland xdg-desktop-portal-hyprland kitty neovim
+
+# Symlinks
+
+ln -sf /usr/local/bin/vi /usr/bin/nvim
+ln -sf /usr/local/bin/vim /usr/bin/nvim
 
 # Enabling System Units
 
