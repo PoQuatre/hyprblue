c02423a6fa009e0580909d428daef18288fa1518
PoQuatre <leflem.mateo2002@gmail.com>
2024-08-06 15:33:11 +0200

    chore: remove `vi` and `vim` symlinks

--- build.sh
+++ build.sh
@@ -8,12 +8,6 @@ RELEASE="$(rpm -E %fedora)"
 
 rpm-ostree install hyprland xdg-desktop-portal-hyprland kitty neovim zsh ripgrep fzf fd-find
 
-# Symlinks
-
-mkdir -p /var/usrlocal/bin
-ln -sf /usr/bin/nvim /var/usrlocal/bin/vi
-ln -sf /usr/bin/nvim /var/usrlocal/bin/vim
-
 # Enabling System Units
 
 systemctl enable podman.socket
