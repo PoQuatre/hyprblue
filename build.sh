2ff12f0914ae3d64c58b6624670196f471e76f5d
PoQuatre <leflem.mateo2002@gmail.com>
2024-08-07 05:34:33 +0200

    feat: add `tuned` to edit power profiles

--- build.sh
+++ build.sh
@@ -8,13 +8,32 @@ RELEASE="$(rpm -E %fedora)"
 
 pkgs=(
   # Desktop Environment
-  hyprland xdg-desktop-portal-hyprland waybar rofi-wayland swww polkit-gnome pinentry-gnome3 breeze-cursor-theme
+  hyprland
+  xdg-desktop-portal-hyprland
+  waybar
+  rofi-wayland
+  swww
+  polkit-gnome
+  pinentry-gnome3
+  breeze-cursor-theme
 
   # Essential Apps
-  kitty neovim thunar thunar-volman thunar-archive-plugin
+  kitty
+  neovim
+  thunar
+  thunar-volman
+  thunar-archive-plugin
+  tuned
 
   # CLI Tools
-  zsh ripgrep fzf fd-find zoxide cliphist gh NetworkManager-tui
+  zsh
+  ripgrep
+  fzf
+  fd-find
+  zoxide
+  cliphist
+  gh
+  NetworkManager-tui
 )
 
 rpm-ostree install "${pkgs[@]}"
